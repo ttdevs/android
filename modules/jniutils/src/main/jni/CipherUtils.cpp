91cb8fb51d5206a57e7e016723cba16d15b0f5ab
ttdevs <wuhuaihu@boohee.com>
2016-04-15 18:18:56 +0800

    修改build.gradle

--- modules/jniutils/src/main/jni/CipherUtils.cpp
+++ modules/jniutils/src/main/jni/CipherUtils.cpp
@@ -1,24 +1,23 @@
-#include <stdlib.h>
-#include <string>
+#include <jni.h>
+// #include <string>
 
 using namespace std;
 
-#include "com_ttdevs_jniutils_CipherUtils.h"
 #include "utils/log.h"
+#include "base64.h"
 
-JNIEXPORT jint JNICALL Java_com_ttdevs_jniutils_CipherUtils_add(JNIEnv *, jclass, jint x, jint y) {
-    std::string input_str("MoreWindows-http://blog.csdn.net/morewindows");
+#include "com_ttdevs_jniutils_CipherUtils.h"
+
+JNIEXPORT jint JNICALL Java_com_ttdevs_jniutils_CipherUtils_add(JNIEnv *env, jclass type, jint x, jint y) {
+    std::string input_str("\"{\\\"bucket\\\":\\\"onetest\\\",\\\"keys\\\":[\\\"/one/2016-08-09/160402b4-0470-49e0-b408-b580675cf910.png\\\",\\\"/bingo/2015-12-09/160402b4-0470-49e0-b408-b580675cf910.png\\\",\\\"/status/2014-10-09/160402b4-0470-49e0-b408-b580675cf910.png\\\",\\\"/food/2014-10-09/160402b4-0470-49e0-b408-b580675cf910.png\\\"]}\"");
     std::string base64_str, output_str;
 
     LOGE("%s", input_str.c_str());
     printf("input_strinput_str");
 
-    // __android_log_print(ANDROID_LOG_INFO,LOG_TAG, "aaa");
-    // Base64Encode(input_str, &base64_str);
-    // cout<<"encode: \n"<<base64_str<<endl;
-    // __android_log_print(ANDROID_LOG_INFO,LOG_TAG,base64_str.c_str());
-    // Base64Decode(base64_str, &output_str);
-    // cout<<"decode: \n"<<output_str<<endl;
-    // __android_log_print(ANDROID_LOG_INFO,LOG_TAG,"Call stringFromJNI!\n");
+    Base64Encode(input_str, &base64_str);
+    LOGE("%s", base64_str.c_str());
+    Base64Decode(base64_str, &output_str);
+    LOGE("%s", output_str.c_str());
     return x + y;
 }
